e0cfc0c8ca8232ff00a3070e6382357fca5917a5
mikepreston17 <michael.n.preston@gmail.com>
2023-10-22 22:19:03 -0500

    Added a weather type for holding results

--- main.go
+++ main.go
@@ -1,14 +1,31 @@
 package main
 
-import "net/http"
+import (
+	"fmt"
+	"io"
+	"net/http"
+)
 
 func main() {
-	// fmt.Println("ready to go")
 
 	res, err := http.Get("https://api.weatherapi.com/v1/current.json?key=4d31bbdd7f954dcb8b600541232310&q=Dallas&aqi=no")
 
 	if err != nil {
 		panic(err)
 	}
+
+	defer res.Body.Close()
+
+	if res.StatusCode != 200 {
+		panic("Weather API not available")
+	}
+
+	body, err := io.ReadAll(res.Body)
+
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println(string(body))
 
 }
